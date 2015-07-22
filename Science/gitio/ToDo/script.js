9a4b1df1334790a294794d3c37ac75c9ee8042ae
simacna <simacna@gmail.com>
2015-07-22 19:31:17 -0400

    initial commit

---
+++ Science/gitio/ToDo/script.js
@@ -0,0 +1,30 @@
+var template = function(text) {
+  return '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
+};
+
+var main = function() {
+
+  $('form').submit(function() {
+      var text = $('#todo');
+      var html = template(text.val());
+      $(html).appendTo('.list');
+      $(text).val('');
+      return false;
+		});
+
+    
+  
+
+
+  $(document).on('click', '.glyphicon-star', function(){
+  	$(this).toggleClass('active');
+  });
+
+  $(document).on('click', '.glyphicon-remove', function(){
+  	$(this).parent().remove();
+  });
+};
+
+$(document).ready(main);
+
+
